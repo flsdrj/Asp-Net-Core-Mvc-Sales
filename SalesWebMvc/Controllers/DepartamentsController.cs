86e3601a9eaef61a6aec38ab342b3aa7a494dbf6
flsdrj <flsd_rj@hotmail.com>
2019-11-12 00:11:45 -0300

    First Model-Controller-View-Departament

--- SalesWebMvc/Controllers/DepartamentsController.cs
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 
 namespace SalesWebMvc.Controllers
 {
@@ -10,7 +11,11 @@ namespace SalesWebMvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            List<Departament> list = new List<Departament>();
+            list.Add(new Departament { Id = 1, Name = "Eletronics" });
+            list.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(list);
         }
     }
 }
--- SalesWebMvc/Models/Departament.cs
+++ SalesWebMvc/Models/Departament.cs
@@ -1,8 +1,13 @@
-﻿namespace SalesWebMvc.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesWebMvc.Models
 {
     public class Departament
     {
+        [Display (Name = "Identificador")]
         public int Id { get; set; }
-        public string Nome { get; set; }
+
+        [Display (Name = "Nome") ]
+        public string Name { get; set; }
     }
 }
